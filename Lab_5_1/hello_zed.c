42d7bd447a10fe64e6548f4b0c60d3e42c4dd7a8
William FJ Yang <37613436+WilliamFJYang@users.noreply.github.com>
2019-06-19 16:28:50 +0800

    Lab5_1 Hello Zed_Board
    Sample driver module example

---
+++ Lab_5_1/hello_zed.c
@@ -0,0 +1,11 @@
+#include <linux/module.h>
+static int hello_init(void)
+{
+	printk(KERN_ALERT "hellow zedboard!!! \n");
+}
+static void hello_exit(void)
+{
+	printk(KERN_ALERT "Close Module!!! \n");
+}
+module_init(hello_init);
+module_exit(hello_exit);
